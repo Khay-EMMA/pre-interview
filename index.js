92ffeca4d0d8cf1f4de68d8c87b0e7e6664d66a2
Njoku Emmanuel <kalunjoku123@gmail.com>
2019-11-22 11:57:49 +0100

    created an object with job requirements and companies

---
+++ index.js
@@ -0,0 +1,13 @@
+//list of campanies registered on the job search platform and their different job requirements
+let jobRequirements = {
+  companyA: ["apartment", "house", "propertyInsurance"],
+  companyB: ["5doorCar", "4doorCar", "driversLicense", "carInsurance"],
+  comapnyC: ["socialSecurityNumber", "workPermit"],
+  companyD: ["apartment", "flat", "house"],
+  comapnyE: ["2doorCar", "3doorCar", "4doorCar", "5doorCar"],
+  companyF: ["scooter", "bike", "motorcycle", "motorcycleInsurane"],
+  comapnyG: ["massageCertification", "liabilityInsurance"],
+  companyH: ["storagePlace", "garage"],
+  comapnyJ: ["paypalAccount"],
+  comapnyK: [null]
+};
